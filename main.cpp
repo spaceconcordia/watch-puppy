94b2e56533bde95a38759769231d28d5ca8abe34
spaceconcordia <spaceconcordia@stripe.encs.concordia.ca>
2012-08-25 14:19:03 -0400

    Added code for reset_process

--- main.cpp
+++ main.cpp
@@ -5,6 +5,10 @@
  * Created on August 24, 2012, 7:13 PM
  */
 #include <string>
+#include <signal.h>
+#include <cstdlib>
+#include <unistd.h>
+#include <cstring>
 
 using namespace std;
 
@@ -12,51 +16,71 @@ const string COMMANDER("/SpaceConcordia/commander.exe"); //process name for the 
 const string DISPATCHER("/SpaceConcordia/dispatcher.exe");
 const int SLEEP_TIME = 300;
 
+int g_dispatcher_alive = 1;
+int g_commander_alive  = 1;
 
-
-
-#include <signal.h>
-#include <cstdlib>
-#include <unistd.h>
-
-using namespace std;
-
+void kill_all_pids(string pids) {       
+    if (!pids.empty()) {
+        pid_t pid = 0;
+        char* pid_str = strtok(pids.c_str(), " ");
+        
+        while(pid_str != NULL) {
+            pid = atoi(pid_str);
+            kill(pid, SIGKILL);
+            pid_str = strtok(NULL, " "); //grab the next string by starting where we left off
+        }    
+    }
+}
 
 void reset_process(const string cmd) {    
-    //get pid for full path
-        
-    //if one or many, kill it or them
+    string command("pidof ");
+    command.append(cmd);
     
-    //start it
+    //clean up running instances of cmd
+    FILE* fp = popen(command, "r");
+    if (fp != NULL) {
+        char pids[255] = "";
+        fgets(pids, sizeof(pids) - 1, fp);
+
+        fflush(fp);
+        pclose(fp);
+
+        kill_all_pids(pids);                                
+    }
+    else {
+        //log errorzor
+    }
         
-}
-int dispatcher_alive = 1;
-int commander_alive = 1;
-
-void sig_handler_USR1(){
-        commander_alive = 0;
-}
-
-void sig_handler_USR2(){
-        dispatcher_alive = 0;
+    //start the process
+    fp = popen(cmd, "r"); 
+    if (fp != NULL) {       
+        pclose(fp);
+    }
+    else {
+        //log errorzor
+    }
 }
 
-            
-    
+void sig_handler_USR1() {
+        g_commander_alive = 0;
+}
 
- int main(){
-    
-        signal(SIGUSR1, sig_handler_USR1);
-        signal(SIGUSR2, sig_handler_USR2);
-           
-        sleep(SLEEP_TIME);
-        
-        if (dispatcher_alive == 1){
-                reset_process(COMMANDER);
-        }
-        if (commander_alive == 1){
-                reset_process(DISPATCHER);
-        }  
+void sig_handler_USR2() {
+        g_dispatcher_alive = 0;
+}
 
-        return 0; 
- }
+int main() {
+    signal(SIGUSR1, sig_handler_USR1);
+    signal(SIGUSR2, sig_handler_USR2);
+
+    sleep(SLEEP_TIME);
+
+    if (g_dispatcher_alive == 1) {
+            reset_process(COMMANDER);
+    } 
+    if (g_commander_alive == 1) {
+            reset_process(DISPATCHER);
+    }  
+
+    return 0; 
+}
