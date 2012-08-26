88ef1f627449fccb7f66360a1b7dc8ddec6f8f54
spaceconcordia <spaceconcordia@stripe.encs.concordia.ca>
2012-08-26 15:56:27 -0400

    Added spawning new processes.

--- main.cpp
+++ main.cpp
@@ -9,35 +9,50 @@
 #include <cstdlib>
 #include <unistd.h>
 #include <cstring>
+#include <iostream>
+#include <signal.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <stdio.h>
+
 
 using namespace std;
 
-const string COMMANDER("/SpaceConcordia/commander.exe"); //process name for the commander
-const string DISPATCHER("/SpaceConcordia/dispatcher.exe");
-const int SLEEP_TIME = 300;
+const string COMMANDER("/home/spaceconcordia/SpaceConcordia/commander.exe"); //process name for the commander
+const string DISPATCHER("/home/spaceconcordia/SpaceConcordia/dispatcher.exe");
+const int SLEEP_TIME = 10;
 
 int g_dispatcher_alive = 1;
 int g_commander_alive  = 1;
 
+char* string_to_char_ptr(string str, int size) {
+    char* result = (char* )malloc(sizeof(char) * size);
+    strcpy(result, str.c_str());
+    return result;
+}
+
 void kill_all_pids(string pids) {       
     if (!pids.empty()) {
         pid_t pid = 0;
-        char* pid_str = strtok(pids.c_str(), " ");
+        char* pids_str = string_to_char_ptr(pids, pids.size());        
+        char* pid_token = strtok(pids_str, " ");        
         
-        while(pid_str != NULL) {
-            pid = atoi(pid_str);
+        while(pid_token != NULL) {
+            pid = atoi(pid_token);
             kill(pid, SIGKILL);
-            pid_str = strtok(NULL, " "); //grab the next string by starting where we left off
+            pid_token = strtok(NULL, " "); //grab the next string by starting where we left off
         }    
+        
+        free(pids_str);
     }
 }
 
-void reset_process(const string cmd) {    
+void reset_process(const string process) {    
     string command("pidof ");
-    command.append(cmd);
+    command.append(process);
     
-    //clean up running instances of cmd
-    FILE* fp = popen(command, "r");
+    //clean up running instances of process
+    FILE* fp = popen(command.c_str(), "r");
     if (fp != NULL) {
         char pids[255] = "";
         fgets(pids, sizeof(pids) - 1, fp);
@@ -49,38 +64,52 @@ void reset_process(const string cmd) {
     }
     else {
         //log errorzor
+    }        
+   
+    pid_t pid = fork();
+    if (pid < 0) {
+        cout << "oh oh";
     }
-        
-    //start the process
-    fp = popen(cmd, "r"); 
-    if (fp != NULL) {       
-        pclose(fp);
-    }
-    else {
-        //log errorzor
-    }
+    else if (pid == 0) {        
+        execl(process.c_str(), NULL);       
+    }          
 }
 
-void sig_handler_USR1() {
+void sig_handler_USR1(int signum) {
+        cout << "reset commander" << endl;
         g_commander_alive = 0;
 }
 
-void sig_handler_USR2() {
+void sig_handler_USR2(int signum) {
+        cout << "reset dispatcher" << endl;
         g_dispatcher_alive = 0;
 }
 
+//-----------------------------------------------------------------------------
+// Note:
+// To test, do not run it from Netbean. Netbeans becomes the parent so when the
+// puppy exits, Netbeans also kill all the children
+//-----------------------------------------------------------------------------
+
 int main() {
+    int sleep_remaining = SLEEP_TIME;
+        
     signal(SIGUSR1, sig_handler_USR1);
-    signal(SIGUSR2, sig_handler_USR2);
-
-    sleep(SLEEP_TIME);
-
-    if (g_dispatcher_alive == 1) {
-            reset_process(COMMANDER);
-    } 
+    signal(SIGUSR2, sig_handler_USR2);            
+    
+    while (sleep_remaining > 0) {
+        sleep_remaining -= 1;             
+        cout << sleep_remaining << endl;
+        cout.flush();
+        sleep(1);
+    }       
+        
     if (g_commander_alive == 1) {
-            reset_process(DISPATCHER);
-    }  
-
-    return 0; 
+        reset_process(COMMANDER);
+    } 
+    if (g_dispatcher_alive == 1) {
+        reset_process(DISPATCHER);
+    }      
+    
+    return 0;   
 }
